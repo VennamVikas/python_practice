a0ead34a654d5de4d166511a4f822b41c690530f
vikas <vennamvikas1@gmail.com>
2025-06-11 18:26:48 +0530

    adding if else statements

---
+++ 1_basic/17_coding_even_num.py
@@ -0,0 +1,8 @@
+#WAP to check the Number is even or odd
+
+num=int(input('enter the number :'))
+
+if(num%2==0):
+    print(f" the {num} is even number ")
+else:
+    print(f"the {num} is odd number")
